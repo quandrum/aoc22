41fa20eb1cc55e99dd85e50fd284fb4d07e40299
Adam Sinnett <quandrum@gmail.com>
2022-12-01 21:59:05 -0800

    Day 2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,15 +1,15 @@
 defmodule AdventOfCode.Day01 do
   def part1(input) do
     Enum.map(input, &elfWeight(&1))
-    |> Enum.sort
+    |> Enum.sort()
     |> Enum.at(-1)
   end
 
   def part2(input) do
     Enum.map(input, &elfWeight(&1))
-    |> Enum.sort
+    |> Enum.sort()
     |> Enum.take(-3)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   def parseInput(input) do
@@ -19,7 +19,7 @@ defmodule AdventOfCode.Day01 do
 
   def elfWeight(elf) do
     String.split(elf, "\n")
-     |> Enum.map(&String.to_integer(&1))
-     |> Enum.sum
+    |> Enum.map(&String.to_integer(&1))
+    |> Enum.sum()
   end
 end
--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,12 +1,66 @@
 defmodule AdventOfCode.Day02 do
   def part1(input) do
+    Enum.map(input, &playHand(&1))
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.sum()
   end
 
   def part2(input) do
+    Enum.map(input, &playResult(&1))
+    |> Enum.map(&Enum.sum(&1))
+    |> Enum.sum()
   end
 
   def parseInput(input) do
     String.trim(input)
     |> String.split("\n")
   end
+
+  def playHand(guide) do
+    String.split(guide, " ")
+    |> (fn [a, b] -> [convert(a), convert(b)] end).()
+    |> (fn [a, b] -> [result(a, b), score(b)] end).()
+  end
+
+  def playResult(guide) do
+    String.split(guide, " ")
+    |> (fn [a, b] -> [convert(a), findDesiredResult(b)] end).()
+    |> (fn [a, b] -> [a, myThrow(a, b)] end).()
+    |> (fn [a, b] -> [result(a, b), score(b)] end).()
+  end
+
+  def convert("A"), do: :rock
+  def convert("B"), do: :paper
+  def convert("C"), do: :scissors
+  def convert("X"), do: :rock
+  def convert("Y"), do: :paper
+  def convert("Z"), do: :scissors
+
+  def findDesiredResult("X"), do: :lose
+  def findDesiredResult("Y"), do: :draw
+  def findDesiredResult("Z"), do: :win
+
+  def result(:rock, :rock), do: 3
+  def result(:rock, :paper), do: 6
+  def result(:rock, :scissors), do: 0
+  def result(:paper, :paper), do: 3
+  def result(:paper, :scissors), do: 6
+  def result(:paper, :rock), do: 0
+  def result(:scissors, :scissors), do: 3
+  def result(:scissors, :rock), do: 6
+  def result(:scissors, :paper), do: 0
+
+  def score(:rock), do: 1
+  def score(:paper), do: 2
+  def score(:scissors), do: 3
+
+  def myThrow(:rock, :win), do: :paper
+  def myThrow(:rock, :draw), do: :rock
+  def myThrow(:rock, :lose), do: :scissors
+  def myThrow(:paper, :win), do: :scissors
+  def myThrow(:paper, :draw), do: :paper
+  def myThrow(:paper, :lose), do: :rock
+  def myThrow(:scissors, :win), do: :rock
+  def myThrow(:scissors, :draw), do: :scissors
+  def myThrow(:scissors, :lose), do: :paper
 end
