a99bea355d1092d9fce7aff20c465f06f4a0b914
Adam Sinnett <quandrum@gmail.com>
2022-12-03 22:09:41 -0800

    Use subset? and disjoint?

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,34 +1,32 @@
 defmodule AdventOfCode.Day04 do
   def part1(work_orders) do
-    Enum.filter(work_orders, &is_complete_overlap(&1))
+    Enum.filter(work_orders, &is_complete_overlap?(&1))
     |> Enum.count()
   end
 
   def part2(work_orders) do
-    Enum.filter(work_orders, &is_partial_overlap(&1))
+    Enum.filter(work_orders, &is_partial_overlap?(&1))
     |> Enum.count()
   end
 
+  def is_complete_overlap?([a, b]), do: MapSet.subset?(a, b) || MapSet.subset?(b, a)
+  def is_partial_overlap?([a, b]), do: !MapSet.disjoint?(a, b)
+
   def parseInput(input) do
-    String.trim(input) 
+    String.trim(input)
     |> String.split("\n")
     |> Enum.map(&work_orders(&1))
   end
 
   def work_orders(work) do
-    [first, second] = String.split(work, ",")
-
-    [
-      Enum.map(String.split(first, "-"), &String.to_integer(&1)),
-      Enum.map(String.split(second, "-"), &String.to_integer(&1))
-    ]
+    String.split(work, ",")
+    |> (fn [first, second] -> [make_set(first), make_set(second)] end).()
   end
 
-  def is_complete_overlap([[a, b], [x, y]]) do
-    (a <= x && b >= y) || (x <= a && y >= b)
-  end
-
-  def is_partial_overlap([[a, b], [x, y]]) do
-    is_complete_overlap([[a,b], [x,y]]) || (x >= a && x <= b) || (y >= a && y <= b)
+  def make_set(range) do
+    String.split(range, "-")
+    |> Enum.map(&String.to_integer(&1))
+    |> (fn [x, y] -> Range.new(x, y) end).()
+    |> MapSet.new()
   end
 end
