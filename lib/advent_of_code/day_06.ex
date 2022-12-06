4c2e20671e1546de2d5d5ab099e13069b9aca045
Adam Sinnett <quandrum@gmail.com>
2022-12-06 09:17:45 -0800

    Day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,12 +1,22 @@
 defmodule AdventOfCode.Day06 do
   def part1(input) do
+    find_first_unique_of_size(input, 4)
   end
 
   def part2(input) do
+    find_first_unique_of_size(input, 14)
+  end
+
+  def find_first_unique_of_size(input, size) do
+    chunk_index =
+      String.to_charlist(input)
+      |> Enum.chunk_every(size, 1)
+      |> Enum.find_index(fn chunk -> MapSet.new(chunk) |> MapSet.size() == size end)
+
+    chunk_index + size
   end
 
   def parseInput(input) do
     String.trim(input)
-    |> String.split("\n")
   end
 end
